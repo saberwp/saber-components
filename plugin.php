6cb01ba6e26886fc19d1ec74c7b28b017a569ee9
Casey Milne <casey.milne@eatbuildplay.com>
2023-02-12 04:15:37 -0600

    Init commit.

---
+++ plugin.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * Plugin Name: Saber Components
+ * Version: 0.0.1
+ */
+
+namespace SaberComponents;
+
+class Plugin {
+
+	public function __construct() {
+
+
+
+	}
+
+}
+
+new Plugin();
